3344cf056399a709bde7fc0f9f6241825ee6da57
Konstantin Smyshliaev <smyshlyaevkonst@gmail.com>
2024-07-07 19:30:25 +0400

    Added booking endpoint and scheme

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI, Query
 from datetime import date
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -13,9 +14,20 @@ def get_hotels(
         has_spa: Optional[bool] = None,
         stars: Optional[int] = Query(None, ge=1, le=5)
 ):
-    return "List of all hotels"
+    pass
 
 
 @app.get("/hotels/{hotel_id}")
 def get_hotel(hotel_id: int):
     return hotel_id
+
+
+class BookingScheme(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/bookings")
+def add_booking(booking: BookingScheme):
+    pass
