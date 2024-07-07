46676bcb6b48b6633d31a77bcefb31f1bc42c6fc
Konstantin Smyshliaev <smyshlyaevkonst@gmail.com>
2024-07-07 19:56:32 +0400

    Added validation for input parameters

--- app/main.py
+++ app/main.py
@@ -1,20 +1,31 @@
 from typing import Optional
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
 from datetime import date
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class HotelsSearchArgs:
+    def __init__(self,
+                 location: str,
+                 date_from: date,
+                 date_to: date,
+                 has_spa: Optional[bool] = None,
+                 stars: Optional[int] = Query(None, ge=1, le=5)
+                 ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
 @app.get("/hotels")
 def get_hotels(
-        location: str,
-        date_from: date,
-        date_to: date,
-        has_spa: Optional[bool] = None,
-        stars: Optional[int] = Query(None, ge=1, le=5)
+    search_args: HotelsSearchArgs = Depends()
 ):
-    pass
+    return search_args
 
 
 @app.get("/hotels/{hotel_id}")
